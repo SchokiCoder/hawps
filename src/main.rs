a22893491f06d2be1309387af1b34074fca69569
SchokiCoder <afschoknecht@gmail.com>
2022-06-20 15:40:31 +0200

    fixed pixel drawing added fixed framerate added keyboard ctl for material spawn

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,47 @@
 use std::vec::Vec;
+use sdl2::keyboard::*;
 use sdl2::event::*;
 use sdl2::pixels::*;
 use sdl2::rect::*;
 
-#[derive(Copy, Clone)]
-pub enum Material {
-	None,
-	Oxygen,
-	CarbonDioxide,
-	Sand,
-	Dirt,
+pub struct Material {
+	name: &'static str,
+	color: Color,
 }
 
-impl Material {
-	pub fn color(&self) -> Color {
-		match self {
-			Material::None => return Color::RGB(0, 0, 0),
-			Material::Oxygen => return Color::RGB(155, 219, 245),
-			Material::CarbonDioxide => return Color::RGB(155, 219, 245),
-			Material::Sand => return Color::RGB(255, 255, 0),
-			Material::Dirt => return Color::RGB(255, 255, 0),
-		}
-	}
-}
+const MAT_COUNT: usize = 5;
+
+const MATERIALS: [Material; MAT_COUNT] = [
+	Material {
+		name: "None",
+		color: Color::RGB(0, 0, 0),
+	},
+
+	Material {
+		name: "Oxygen",
+		color: Color::RGB(155, 219, 245),
+	},
+
+	Material {
+		name: "CarbonDioxide",
+		color: Color::RGB(155, 219, 245),
+	},
+
+	Material {
+		name: "Sand",
+		color: Color::RGB(255, 255, 0),
+	},
+
+	Material {
+		name: "Dirt",
+		color: Color::RGB(255, 255, 0),
+	},
+];
+
+type MatIndex = usize;
 
 pub struct Sandbox {
-	mats: Vec<Vec<Material>>,
+	mats: Vec<Vec<MatIndex>>,
 }
 
 impl Sandbox {
@@ -35,7 +51,7 @@ impl Sandbox {
 			mats: Vec::new(),
 		};
 
-		result.mats = vec![vec![Material::None; height]; width];
+		result.mats = vec![vec![0; height]; width];
 		
 		return result;
 	}
@@ -43,6 +59,7 @@ impl Sandbox {
 
 fn main() {
 	// init
+	const FRAME_DELTA: u32 = 1_000_000_000 / 30;
 	const WIDTH: usize = 200;
 	const HEIGHT: usize = 200;
 	
@@ -58,20 +75,49 @@ fn main() {
 	let mut canvas = window.into_canvas().build().unwrap();
 
 	// mainloop
+	let mut ts_now: std::time::Instant;
+	let mut ts_draw = std::time::Instant::now();
 	let mut sandbox = Sandbox::new(WIDTH, HEIGHT);
 
+	let mut selected_mat: MatIndex = 0;
+
 	'mainloop: loop {
+		// time
+		ts_now = std::time::Instant::now();
 	
 		// events
 		for event in sdlsys_event.poll_iter() {
 		
 			match event {
+
+				Event::KeyUp {keycode, ..} => {
+					if keycode.is_some() == false {
+						break;
+					}
+					
+					match keycode.unwrap() {
+						Keycode::Up => {
+							if selected_mat > 0 {
+								selected_mat -= 1;
+							}
+						},
+
+						Keycode::Down => {
+							if selected_mat < (MAT_COUNT - 1) {
+								selected_mat += 1;
+							}
+						},
+
+						_ => ()
+					}
+				}
+
 				Event::MouseMotion {mousestate, x, y, ..} => {
 					// if lmb is held down while mousemotion
 					if mousestate.left() {
 					
 						// spawn selected material
-						sandbox.mats[x as usize][y as usize] = Material::Sand;
+						sandbox.mats[x as usize][y as usize] = selected_mat;
 					}
 				},
 
@@ -81,23 +127,20 @@ fn main() {
 			}
 		}
 
-		// draw bg
-		canvas.set_draw_color(Color::RGB(0, 0, 0));
-		canvas.fill_rect(
-			Rect::new(
-				0,
-				0, 
-				sandbox.mats.len() as u32,
-				sandbox.mats[0].len() as u32)).unwrap();
-
-		// pixels
-		for x in 0..sandbox.mats.len() {
-			for y in 0..x {
-				canvas.set_draw_color(sandbox.mats[x][y].color());
-				canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+		// draw
+		if ts_now > (ts_draw + std::time::Duration::new(0, FRAME_DELTA)) {
+			// pixels
+			for x in 0..sandbox.mats.len() {
+				for y in 0..sandbox.mats[0].len() {
+					canvas.set_draw_color(MATERIALS[sandbox.mats[x][y]].color);
+					canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+				}
 			}
+			
+			canvas.present();
+
+			// update ts
+			ts_draw = ts_now;
 		}
-		
-		canvas.present();
 	}
 }
