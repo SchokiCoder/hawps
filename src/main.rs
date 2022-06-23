b14704b57d01cae23f8af30074bbd7102e2d0d3e
SchokiCoder <afschoknecht@gmail.com>
2022-06-23 11:45:54 +0200

    added drawing selected material added gravity added timescale wip draw timescale

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::vec::Vec;
 use sdl2::keyboard::*;
 use sdl2::event::*;
 use sdl2::pixels::*;
@@ -9,74 +8,92 @@ use sdl2::rect::*;
 pub struct Material {
 	name: &'static str,
 	color: Color,
+	weight: u32,
 }
 
-const MAT_COUNT: usize = 5;
+// game stuff
+const STD_TICK_PS: u32 = 30;
+const TICK_CHANGE_STEP: u32 = 10;
 
-const MATERIALS: [Material; MAT_COUNT] = [
+// window stuff
+const FRAME_DELTA: u32 = 1_000_000_000 / 30;
+const WINDOW_W: u32 = 400;
+const WINDOW_H: u32 = 400;
+
+// world size
+const SANDBOX_X: u32 = 200;
+const SANDBOX_Y: u32 = 200;
+const SANDBOX_W: u32 = 200;
+const SANDBOX_H: u32 = 200;
+
+// hud positions
+const FONT_SIZE: u32 = 24;
+
+const HUD_MATERIAL_X: u32 = 10;
+const HUD_MATERIAL_Y: u32 = 10;
+
+const HUD_TICK_PS_X: u32 = HUD_MATERIAL_X;
+const HUD_TICK_PS_Y: u32 = HUD_MATERIAL_Y + FONT_SIZE + 5;
+
+// material properties
+const MATERIALS: [Material; 5] = [
 	Material {
 		name: "None",
 		color: Color::RGB(0, 0, 0),
+		weight: 0,
 	},
 
 	Material {
 		name: "Oxygen",
 		color: Color::RGB(155, 219, 245),
+		weight: 1,
 	},
 
 	Material {
 		name: "CarbonDioxide",
 		color: Color::RGB(155, 219, 245),
+		weight: 2,
 	},
 
 	Material {
 		name: "Sand",
 		color: Color::RGB(255, 255, 0),
+		weight: 50,
 	},
 
 	Material {
 		name: "Dirt",
 		color: Color::RGB(37, 25, 13),
+		weight: 50,
 	},
 ];
 
 type MatIndex = usize;
 
-pub struct Sandbox {
-	mats: Vec<Vec<MatIndex>>,
+fn update_tick_ps(
+	tick_ps: &mut u32,
+	tick_ps_surface: &mut Surface,
+	tick_ps_texture: &mut Texture,
+	font: &sdl2::ttf::Font,
+	texture_creator: &TextureCreator<WindowCanvas>) {
+
+	*tick_ps_surface = font
+		.render(tick_ps.to_string().as_str())
+		.solid(Color::RGB(255, 255, 255))
+		.unwrap();
+
+	*tick_ps_texture = tick_ps_surface
+		.as_texture(&texture_creator)
+		.unwrap();
 }
 
-impl Sandbox {
-	pub fn new(width: u32, height: u32) -> Sandbox {
-		// reserve memory and initialize values
-		let mut result = Sandbox {
-			mats: Vec::new(),
-		};
-
-		result.mats = vec![vec![0; height as usize]; width as usize];
-		
-		return result;
-	}
-}
-
-fn main() {
-	const FRAME_DELTA: u32 = 1_000_000_000 / 30;
-	const WINDOW_W: u32 = 400;
-	const WINDOW_H: u32 = 400;
+fn main() {	
 	let window_rect = Rect::new(0, 0, WINDOW_W, WINDOW_H);
+	
+	let mut rect_draw = Rect::new(0, 0, 0, 0);
 
-	const SANDBOX_X: u32 = 200;
-	const SANDBOX_Y: u32 = 200;
-	const SANDBOX_W: u32 = 200;
-	const SANDBOX_H: u32 = 200;
-
-	const HUD_MATERIAL_X: u32 = 10;
-	const HUD_MATERIAL_Y: u32 = 10;
-	let mut rect_selected_mat = Rect::new(
-		HUD_MATERIAL_X as i32,
-		HUD_MATERIAL_Y as i32,
-		100,
-		100);
+	let mut tick_ps = STD_TICK_PS;
+	let mut tick_delta = 1_000_000_000 / tick_ps;
 
 	// init sdl
 	let sdl = sdl2::init().unwrap();
@@ -94,27 +111,77 @@ fn main() {
 	// init ttf
 	let ttf = sdl2::ttf::init().unwrap();
 
-	let font = ttf.load_font("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 24).unwrap();
+	let font = ttf.load_font(
+		"/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf",
+		FONT_SIZE as u16).unwrap();
 
-	// text textures	
-	let mat_surfaces: [Surface; MAT_COUNT];
-	let mat_textures: [Texture; MAT_COUNT];
+	// text textures
+	let tick_ps_surface = font
+		.render(tick_ps.to_string().as_str())
+		.solid(Color::RGB(255, 255, 255))
+		.unwrap();
 
-	for i in 0..MAT_COUNT {
-		mat_surfaces[i] = font
-			.render(MATERIALS[i].name)
+	let tick_ps_texture = tick_ps_surface
+		.as_texture(&texture_creator)
+		.unwrap();
+	
+	// this looks terrible but rust wont let init the array in a loop	
+	let mat_surfaces: [Surface; MATERIALS.len()] = [
+		font
+			.render(MATERIALS[0].name)
 			.solid(Color::RGB(255, 255, 255))
-			.unwrap();
-				
-		mat_textures[i] = mat_surfaces[i]
+			.unwrap(),
+
+		font
+			.render(MATERIALS[1].name)
+			.solid(Color::RGB(255, 255, 255))
+			.unwrap(),
+
+		font
+			.render(MATERIALS[2].name)
+			.solid(Color::RGB(255, 255, 255))
+			.unwrap(),
+
+		font
+			.render(MATERIALS[3].name)
+			.solid(Color::RGB(255, 255, 255))
+			.unwrap(),
+
+		font
+			.render(MATERIALS[4].name)
+			.solid(Color::RGB(255, 255, 255))
+			.unwrap(),
+	];
+	
+	let mat_textures: [Texture; MATERIALS.len()] = [
+		mat_surfaces[0]
 			.as_texture(&texture_creator)
-			.unwrap();
-	}
+			.unwrap(),
+
+		mat_surfaces[1]
+			.as_texture(&texture_creator)
+			.unwrap(),
+
+		mat_surfaces[2]
+			.as_texture(&texture_creator)
+			.unwrap(),
+
+		mat_surfaces[3]
+			.as_texture(&texture_creator)
+			.unwrap(),
+
+		mat_surfaces[4]
+			.as_texture(&texture_creator)
+			.unwrap(),
+	];
 
 	// mainloop
 	let mut ts_now: std::time::Instant;
+	let mut ts_tick = std::time::Instant::now();
 	let mut ts_draw = std::time::Instant::now();
-	let mut sandbox = Sandbox::new(SANDBOX_W, SANDBOX_H);
+	
+	let mut sandbox_mat = [[0 as MatIndex; SANDBOX_H as usize]; SANDBOX_W as usize];
+	let mut sandbox_moved = [[false; SANDBOX_H as usize]; SANDBOX_W as usize];
 
 	let mut selected_mat: MatIndex = 0;
 
@@ -140,9 +207,21 @@ fn main() {
 						},
 
 						Keycode::Down => {
-							if selected_mat < (MAT_COUNT - 1) {
+							if selected_mat < (MATERIALS.len() - 1) {
 								selected_mat += 1;
 							}
+						},
+
+						Keycode::Left => {
+							if tick_ps > TICK_CHANGE_STEP {
+								tick_ps -= TICK_CHANGE_STEP;
+								tick_delta = 1_000_000_000 / tick_ps;
+							}
+						},
+
+						Keycode::Right => {
+							tick_ps += TICK_CHANGE_STEP;
+							tick_delta = 1_000_000_000 / tick_ps;
 						},
 
 						_ => ()
@@ -154,13 +233,45 @@ fn main() {
 					if mousestate.left() {
 					
 						// spawn selected material
-						sandbox.mats[x as usize][y as usize] = selected_mat;
+						sandbox_mat[x as usize][y as usize] = selected_mat;
 					}
 				},
 
 				Event::Quit {..} => break 'mainloop,
 				
 				_ => ()
+			}
+		}
+
+		// physics tick
+		if ts_now > (ts_tick + std::time::Duration::new(0, tick_delta)) {
+			// gravity
+			for x in 0..SANDBOX_W as usize {
+				for y in 0..SANDBOX_H as usize - 1 as usize {
+				
+					if sandbox_moved[x][y] == false {
+						if MATERIALS[sandbox_mat[x][y]].weight >
+							MATERIALS[sandbox_mat[x][y + 1]].weight {
+							
+							let temp = sandbox_mat[x][y];
+							sandbox_mat[x][y] = sandbox_mat[x][y + 1];
+							sandbox_mat[x][y + 1] = temp;
+
+							sandbox_moved[x][y] = true;
+							sandbox_moved[x][y + 1] = true;
+						}
+					}
+				}
+			}
+
+			// update ts
+			ts_tick = ts_now;
+
+			// reset moved flags
+			for x in 0..SANDBOX_W as usize {
+				for y in 0..SANDBOX_H as usize {
+					sandbox_moved[x][y] = false;
+				}
 			}
 		}
 
@@ -171,11 +282,22 @@ fn main() {
 			canvas.fill_rect(window_rect).unwrap();
 
 			// hud
-			rect_selected_mat.w = mat_surfaces[selected_mat].rect().w;
-			rect_selected_mat.h = mat_surfaces[selected_mat].rect().h;
+			rect_draw.x = HUD_MATERIAL_X as i32;
+			rect_draw.y = HUD_MATERIAL_Y as i32;
+			rect_draw.w = mat_surfaces[selected_mat].rect().w;
+			rect_draw.h = mat_surfaces[selected_mat].rect().h;
 			
 			canvas
-				.copy(&mat_textures[selected_mat], None, Some(rect_selected_mat))
+				.copy(&mat_textures[selected_mat], None, Some(rect_draw))
+				.unwrap();
+
+			rect_draw.x = HUD_TICK_PS_X as i32;
+			rect_draw.y = HUD_TICK_PS_Y as i32;
+			rect_draw.w = tick_ps_surface.rect().w;
+			rect_draw.h = tick_ps_surface.rect().h;
+						
+			canvas
+				.copy(&tick_ps_texture, None, Some(rect_draw))
 				.unwrap();
 
 			// pixels
@@ -184,7 +306,7 @@ fn main() {
 			
 			for x in 0..SANDBOX_W as usize {
 				for y in 0..SANDBOX_H as usize {
-					canvas.set_draw_color(MATERIALS[sandbox.mats[x][y]].color);
+					canvas.set_draw_color(MATERIALS[sandbox_mat[x][y]].color);
 					canvas.draw_point(Point::new(dx, dy)).unwrap();
 
 					dy += 1;
